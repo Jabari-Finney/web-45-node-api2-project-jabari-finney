15bc45720868e8d72a91014995bec9cee8332b8a
Jabari-Finney <jabaritf@gmail.com>
2021-08-31 15:02:55 -0400

    wip

--- api/server.js
+++ api/server.js
@@ -1,4 +1,16 @@
-const express = require("Express")
+const express = require("express")
+
+const server = express()
+
+server.use(express.json())
 
 // implement your server here
 // require your posts router and connect it here
+
+server.use('*', (req, res) => {
+    res.status(404).json({
+        message: 'not found'
+    })
+})
+
+module.exports = server;
